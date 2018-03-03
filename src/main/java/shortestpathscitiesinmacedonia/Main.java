4c6bcd3988417ab570e85bc73efc7291d7071d4c
Hristijan <petreski.hristijan7@gmail.com>
2018-03-03 01:42:49 +0100

    Create basic program layout

--- src/main/java/shortestpathscitiesinmacedonia/Main.java
+++ src/main/java/shortestpathscitiesinmacedonia/Main.java
@@ -1,5 +1,8 @@
 package shortestpathscitiesinmacedonia;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * A GUI Swing application used to find the shortest path between the cities
  * in Macedonia
@@ -9,7 +12,47 @@ package shortestpathscitiesinmacedonia;
  */
 public class Main {
 	public static void main(String[] args) {
-
+		
+		});
 	}
+}
 
+class ShortestPathFrame extends JFrame{
+	private static final int DEFAULT_WIDTH = 400;
+	private static final int DEFAULT_HEIGHT = 300;
+	private JLabel fromLabel;
+	private JLabel toLabel;
+	private JComboBox fromCombo;
+	private JComboBox toCombo;
+	private JLabel routeLabel;
+	private JLabel distanceLabel;
+	private JPanel mainPanel;
+	private JPanel secPanel;
+	
+	public ShortestPathFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		fromLabel = new JLabel("From:");
+		toLabel = new JLabel("To:");
+		fromCombo = new JComboBox();
+		toCombo = new JComboBox();
+		routeLabel = new JLabel("The shortest route is: ");
+		distanceLabel = new JLabel("The distance is: ");
+		
+		mainPanel = new JPanel(new GridLayout(4,1));
+		secPanel = new JPanel(new GridLayout(1,2));
+		
+		secPanel.add(fromLabel);
+		secPanel.add(fromCombo);
+		mainPanel.add(secPanel);
+		
+		secPanel = new JPanel(new GridLayout(1,2));
+		secPanel.add(toLabel);
+		secPanel.add(toCombo);
+		mainPanel.add(secPanel);
+		
+		mainPanel.add(routeLabel);
+		mainPanel.add(distanceLabel);
+		
+	}
 }
