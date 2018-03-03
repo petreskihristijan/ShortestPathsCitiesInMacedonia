98c18fae9150667f0f05a35553f74ac8dcc5054e
Hristijan <petreski.hristijan7@gmail.com>
2018-03-03 01:09:09 +0100

    Initialize Main class

---
+++ src/main/java/shortestpathscitiesinmacedonia/Main.java
@@ -0,0 +1,15 @@
+package shortestpathscitiesinmacedonia;
+
+/**
+ * A GUI Swing application used to find the shortest path between the cities
+ * in Macedonia
+ * @version 1.0 03-03-2018
+ * @author Hristijan
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+}
