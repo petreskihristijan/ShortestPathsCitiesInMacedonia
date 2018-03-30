9831c1cdee1f6716dc13e8a2381e4ac623e041d2
Hristijan <petreski.hristijan7@gmail.com>
2018-03-30 20:54:28 +0200

    Update the AdjacencyList class

--- src/main/java/shortestpathscitiesinmacedonia/AdjacencyList.java
+++ src/main/java/shortestpathscitiesinmacedonia/AdjacencyList.java
@@ -1,11 +1,38 @@
 package shortestpathscitiesinmacedonia;
 
-public class AdjacencyList<T> implements GraphContainer<T> {
+import java.util.List;
+
+public class AdjacencyList<T> implements GraphContainer<T>, GraphDataStructure<T> {
+	private Bag<T>[] container;
 	
+	public AdjacencyList(int elements) {
+		container = new Bag[elements];
+		for(int i = 0; i < elements; i++) {
+			container[i] = new Bag<>();
+		}
+	}
 
 	@Override
 	public void add(T vertex) {
 		
+	}
+
+	@Override
+	public boolean hasAdjacentVertex(T vertex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<T> listAdjacentVertecies(T vertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int vertexDegree(T vertex) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
